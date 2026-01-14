36c3b1c28b1aa74fc0b07b5f8c0a62b131c1c412
masrul-hadi <masrul.hadi.id@gmail.com>
2026-01-14 20:15:41 +0700

    belajar: belajar variable let

---
+++ variable-declarations/let.js
@@ -0,0 +1,7 @@
+let nama = "masrul hadi"
+let usia = 19
+let statuse = "hidup"
+
+console.log("hai nama saya ", nama, ", saya berusia ", usia, ", dan saya masih ", statuse)
+
+// let merupakan tipe data yang dinamis, sehingga dapat diubah ubah nilainya mengikuti perkembangan sistem
